889d14786c4a0ce92c7415e13e9807b35c9b5af8
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-06-09 18:43:15 +0900

    プロジェクト ファイルを追加します。

---
+++ CollisionDetectionTest/CollisionDetectionTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CollisionDetectionTest
+{
+	TEST_CLASS(CollisionDetectionTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
